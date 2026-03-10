35cb1a216fe36a9989c8624d8ef28cb0803f4f8e
Storme Simon <simon.storme@student.vinci.be>
2026-03-10 10:06:46 +0100

    add code in constructor Graph

--- src/classes/Graph.java
+++ src/classes/Graph.java
@@ -4,16 +4,19 @@ import java.io.File;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.SynchronousQueue;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class Graph {
 
 	//ATTRIBUT ?
 	//TODO
-  protected Map<String, Noeud> correspondanceIdNoeud;
+  protected Map<Integer, Noeud> correspondanceIdNoeud;
   protected Map<String, Arc> correspondanceNomRue;
 
 
@@ -24,7 +27,19 @@ public class Graph {
       DocumentBuilderFactory docBuildFact = DocumentBuilderFactory.newInstance();
       DocumentBuilder docBuild = docBuildFact.newDocumentBuilder();
       Document doc = docBuild.parse(csv);
-
+      NodeList noeuds = doc.getElementsByTagName("id");
+      for (int i = 0; i < noeuds.getLength(); i++) {
+        Node noeud = noeuds.item(i);
+        Element elNoeud = (Element) noeud;
+        int id = Integer.parseInt(elNoeud.getAttribute("id"));
+        String name = elNoeud.getAttribute("name");
+        double lat = Double.parseDouble(elNoeud.getAttribute("lat"));
+        double lon = Double.parseDouble(elNoeud.getAttribute("lon"));
+        int alt = Integer.parseInt(elNoeud.getAttribute("alt"));
+        Noeud n = new Noeud(id,lat,lon,name,alt);
+        correspondanceIdNoeud.put(id,n);
+        //ajouter : ajouterNoeud();
+      }
     }
 
     public Localisation[] determinerZoneInondee(long[] idsOrigin,double epsilon) {
